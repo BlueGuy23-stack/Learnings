7092d5db4c7e36aac97d7eb110c9bbdbab145876
BlueGuy23-stack <your.email@example.com>
2024-08-18 07:49:01 +0530

    first commit

---
+++ main.cpp
@@ -0,0 +1,58 @@
+/******************************************************************
+   A program with somoe functions and comments
+********************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{   
+    // Write a C++ program that outputs the following text on screen:
+    cout<< "Oh what\na happy day!\nOh yes ,\nwhat a happy day!" << endl;
+
+    return 0;    
+}
+
+
+/*
+I will pasting my all code which i am practicing 
+#include<iostream>
+using namespace std;
+void line();
+void messageShow();        // prototype
+
+int main()
+{   
+    cout<< "Hello! The program starts inmain() ."
+        << endl;
+    line();
+    messageShow();
+    line();
+    cout<< "At the end of main() . "<< endl;
+
+    return 0;    
+}
+
+void line()
+{
+    cout<< "-------------------------------------------------" << endl;
+}
+
+void messageShow()
+{
+    cout<< "In function message() ." << endl;
+}
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{   
+    // Write a C++ program that outputs the following text on screen:
+    cout<< "Oh what\na happy day!\nOh yes ,\nwhat a happy day!" << endl;
+
+    return 0;    
+}
+
+*/
